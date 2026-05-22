f94bea608c5943bd95bdda0ef441ada516442c66
wizzybangbang <oznolaris@gmail.com>
2026-05-23 04:45:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ class Ping(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"alive")
+        self.wfile.write(b"ok")
 
     def log_message(self, *args):
         pass
